792b07d32541e7acaa9afb8d58b7df6cdc156c4c
ayoubzann <ayoubzann@gmail.com>
2024-01-18 14:18:47 +0100

    Refactor

--- Frontend/MomentumFrontend/src/comps/Pages/WorkoutCrudPage/CreateExercise.tsx
+++ Frontend/MomentumFrontend/src/comps/Pages/WorkoutCrudPage/CreateExercise.tsx
@@ -3,13 +3,12 @@ import React from 'react'
 const CreateExercise = () => {
 
 
-    
+
   return (
     <div>
     <h2 className="text-2xl">Create an exercise</h2>
     <form className="flex flex-col">
       <label>
-        {" "}
         Exercise name:
         <input
           className="rounded-lg p-2 bg-slate-200"
@@ -17,7 +16,6 @@ const CreateExercise = () => {
         />
       </label>
       <label>
-        {" "}
         Intensity (1-10):
         <input
           className="rounded-lg p-2 bg-slate-200"
@@ -25,7 +23,6 @@ const CreateExercise = () => {
         />
       </label>
       <label>
-        {" "}
        Sets:
         <input
           className="rounded-lg p-2 bg-slate-200"
@@ -33,7 +30,6 @@ const CreateExercise = () => {
         />
       </label>
       <label>
-        {" "}
         Reps:
         <input
           className="rounded-lg p-2 bg-slate-200"
