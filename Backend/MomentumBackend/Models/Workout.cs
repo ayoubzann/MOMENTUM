a81d37ca39b96e37e29054895f06d64fc3afe4f8
ayoubzann <ayoubzann@gmail.com>
2024-01-18 08:45:50 +0100

    Added: Workout model

--- Backend/MomentumBackend/Models/Workout.cs
+++ Backend/MomentumBackend/Models/Workout.cs
@@ -1,1 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MomentumBackend.Models;
+
+public class Workout
+{
+    [Key]
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Intensity { get; set; }
+    public string Level { get; set; }
+}
