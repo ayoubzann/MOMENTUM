4096bb1b3bb02ae895d096ea623ef1be8d20bba8
ayoubzann <ayoubzann@gmail.com>
2024-01-18 15:49:42 +0100

    Added: Post method

--- Frontend/MomentumFrontend/src/utils.tsx
+++ Frontend/MomentumFrontend/src/utils.tsx
@@ -1,8 +1,26 @@
 
 
 const baseUrl = "http://localhost:5086/Momentum/api/";
+
 export const getAllWorkouts = async () => {
     const res = await fetch(baseUrl + "getAllWorkouts");
     const data = await res.json();
     return await data;
     };
+export const getAllExercises = async () => {
+    const res = await fetch(baseUrl + "getAllExercises");
+    const data = await res.json();
+    return await data;
+    };
+
+    export const postWorkout = async(workoutToAdd) => {
+        const res = await fetch(baseUrl + "workouts/create", {
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json",
+            },
+            body: JSON.stringify(workoutToAdd)
+        });
+
+        return res;
+    }
