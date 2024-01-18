41a95f44247b56c27552afcc8dee6f7bd3191b10
ayoubzann <ayoubzann@gmail.com>
2024-01-18 19:14:24 +0100

    Added: Types

--- Frontend/MomentumFrontend/src/comps/Pages/WorkoutListPage/WorkoutCard.tsx
+++ Frontend/MomentumFrontend/src/comps/Pages/WorkoutListPage/WorkoutCard.tsx
@@ -1,16 +1,14 @@
-import React from "react";
-import ExerciseList from "./ExerciseList";
-import { deleteWorkout } from "../../../utils";
+import React from 'react';
+import ExerciseList from './ExerciseList';
+import { WorkoutCardProps } from '../../../utils';
 
-const WorkoutCard = ({ data, editor, onDelete }) => {
+const WorkoutCard: React.FC<WorkoutCardProps> = ({ data, editor, onDelete }) => {
   return (
     <div className="border p-2 m-4">
-      <h2 className="text-2xl"> {data.workoutName}</h2>
+      <h2 className="text-2xl">{data.workoutName}</h2>
       <h3 className="text-xl">Intensity: {data.workoutIntensity}</h3>
       <h3 className="text-xl">Workout level: {data.workoutLevel}</h3>
-      {editor ? (
-        <button onClick={() => onDelete(data.workoutName)}>Delete</button>
-      ) : null}
+      {editor && <button onClick={() => onDelete(data.workoutName)}>Delete</button>}
       <ExerciseList data={data.exercises} />
     </div>
   );
