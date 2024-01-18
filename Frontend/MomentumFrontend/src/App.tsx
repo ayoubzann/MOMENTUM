3dd256a552194db80b1b72a37c04b672c7bb4720
ayoubzann <ayoubzann@gmail.com>
2024-01-18 14:16:54 +0100

    Added: routes

--- Frontend/MomentumFrontend/src/App.tsx
+++ Frontend/MomentumFrontend/src/App.tsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import WorkoutListPage from './comps/Pages/WorkoutListPage'
+import WorkoutListPage from './comps/Pages/WorkoutListPage/WorkoutListPage'
+import WorkoutCrudPage from './comps/Pages/WorkoutCrudPage/WorkoutCrudPage'
 
 function App() {
 
   return (
     <>
     <Routes>
-      <Route path="/workoutList" element={<WorkoutListPage/>}/>
+      <Route path="/Workouts" element={<WorkoutListPage/>}/>
+      <Route path="AddWorkouts" element={<WorkoutCrudPage/>}/>
     </Routes>
     </>
   )
