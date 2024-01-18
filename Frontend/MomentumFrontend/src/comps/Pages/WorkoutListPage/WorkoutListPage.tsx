ccac6a9c1002bde17a03e354f9697484a8b59ce6
ayoubzann <ayoubzann@gmail.com>
2024-01-18 18:50:40 +0100

    Added: Filtered workouts

--- Frontend/MomentumFrontend/src/comps/Pages/WorkoutListPage/WorkoutListPage.tsx
+++ Frontend/MomentumFrontend/src/comps/Pages/WorkoutListPage/WorkoutListPage.tsx
@@ -1,11 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { deleteWorkout, getAllWorkouts } from "../../../utils";
 import WorkoutList from "./WorkoutList";
 import { Link } from "react-router-dom";
+import MoodContext from "../../NumberContext";
 
 const WorkoutListPage = () => {
   const [workouts, setWorkouts] = useState([]);
   const [editor, setEditor] = useState(false);
+  const { mood } = useContext(MoodContext);
 
   const handleDelete = async (workoutName) => {
     try {
@@ -14,6 +16,16 @@ const WorkoutListPage = () => {
       setWorkouts(updatedWorkouts);
     } catch (error) {
       console.error("Error deleting workout:", error);
+    }
+  };
+
+  const filterWorkouts = () => {
+    if (mood <= 4) {
+      return workouts.filter((workout) => workout.workoutIntensity <= 4);
+    } else if (mood <= 7) {
+      return workouts.filter((workout) => workout.workoutIntensity <= 7);
+    } else {
+      return workouts;
     }
   };
 
@@ -26,11 +38,13 @@ const WorkoutListPage = () => {
     fetchData();
   }, []);
 
+  const filteredWorkouts = filterWorkouts();
+
   return (
     <>
       <Link to="/AddWorkouts"> Add Workout</Link>
-      <button onClick={()=>setEditor(!editor)}> Edit workout list </button>
-      <WorkoutList data={workouts} editor={editor} onDelete={handleDelete} />
+      <button onClick={() => setEditor(!editor)}> Edit workout list </button>
+      <WorkoutList data={filteredWorkouts} editor={editor} onDelete={handleDelete} />
     </>
   );
 };
